b36fe68aaa7523a067d208fd2f94429201fb08a9
JrinconRG <juana.876@hotmail.com>
2026-03-26 13:39:51 -0500

    feat:Lógica de peticiones HTTP

---
+++ app/services/useService.ts
@@ -0,0 +1,19 @@
+import axiosFetch from "../lib/axios";
+import {Country} from "../types/countries"
+
+export const getCountries = async():Promise<Country[]> => {
+    const response = await axiosFetch.get<Country[]>('all?fields=name,capital,currencies,flags,latlng');
+    return response.data;
+}
+
+const FIELDS = 'fields=name,capital,currencies,flags,latlng,region'
+export const getCountriesByName = async( name:string):Promise<Country[]>=>{
+    const response = await axiosFetch.get<Country[]>(`name/${name}?${FIELDS}`);
+    return response.data;
+
+}
+
+export const getCountriesFilterByRegion = async(region:string):Promise<Country[]> =>{
+    const response = await  axiosFetch.get<Country[]>(`region/${region}?${FIELDS}`);
+    return response.data;
+}
